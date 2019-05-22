fcf35940643ffca71511fe24d8c6b3c657eb2892
Tony Su <peitaosu@163.com>
2019-05-22 22:53:17 +0800

    add skeleton

--- yspace/yspace/urls.py
+++ yspace/yspace/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^user', views.user),
+    url(r'^event', views.event),
+    url(r'^manage', views.manage)
 ]
---
+++ yspace/yspace/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.conf import settings
+import os
+import sys
+import subprocess
+import json
+import sqlite3
+from sqlite3 import OperationalError
+
+def event(request):
+    pass
+
+def user(request):
+    pass
+
+def manage(request):
+    pass
