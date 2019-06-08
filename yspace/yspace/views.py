5fd6bde9cf2708b880735ec1fe7eedbba0563f08
Tony Su <peitaosu@163.com>
2019-06-08 10:02:19 +0800

    user should be able to register, login and logout

--- yspace/yspace/views.py
+++ yspace/yspace/views.py
@@ -26,7 +26,14 @@ def event(request, action):
         return render(request, 'index.html', context)
 
 def user(request, action):
-    pass
+    if action == "register":
+        pass
+    elif action == "login":
+        pass
+    elif action == "logout":
+        pass
+    else:
+        pass
 
 def manage(request):
     pass
