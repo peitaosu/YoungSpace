e8f2410d656d7838c8722dfc58cdc70c0a9c0b2f
Tony Su <peitaosu@163.com>
2019-06-15 22:15:48 +0800

    add event model

--- yspace/yspace/models.py
+++ yspace/yspace/models.py
@@ -8,3 +8,12 @@ class User(models.Model):
     def __str__(self):
         return self.email
 
+class Event(models.Model):
+    eid = models.IntegerField(unique=True)
+    title = models.CharField(max_length=256)
+    description = models.CharField(max_length=256)
+    picture = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.title
+
