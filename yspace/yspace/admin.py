f50e30776c7151068a6baffcb0ffb8e1d926043c
Tony Su <peitaosu@163.com>
2019-06-18 18:11:05 +0800

    fix table didn't be created issue

--- yspace/yspace/admin.py
+++ yspace/yspace/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Event, Keyword, Event_Keyword, User_Keyword, User_Event, About
 
 admin.site.register(User)
+admin.site.register(Event)
+admin.site.register(Keyword)
+admin.site.register(Event_Keyword)
+admin.site.register(User_Keyword)
+admin.site.register(User_Event)
+admin.site.register(About)
