329519a46098df60b1de69c181233c2c2521bf36
Tony Su <peitaosu@163.com>
2019-06-18 16:57:53 +0800

    update models

--- yspace/yspace/models.py
+++ yspace/yspace/models.py
@@ -4,18 +4,69 @@ from django.db import models
 class User(models.Model):
     password = models.CharField(max_length=256)
     email = models.EmailField(unique=True)
+    name = models.CharField(max_length=64)
+    age = models.IntegerField()
+    USER_GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    gender = models.CharField(
+        max_length=1,
+        choices=USER_GENDER_CHOICES,
+        default='M',
+    )
+    career = models.CharField(max_length=64)
+    biography = models.CharField(max_length=256)
+    picture = models.CharField(max_length=256)
+    register_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.email
 
 class Event(models.Model):
     eid = models.IntegerField(unique=True)
-    title = models.CharField(max_length=256)
+    title = models.CharField(max_length=64)
+    sub_title = models.CharField(max_length=128)
+    short_description = models.CharField(max_length=128)
     description = models.CharField(max_length=256)
     picture = models.CharField(max_length=256)
+    content = models.TextField()
+    start_time = models.DateTimeField()
+    duration = models.DurationField()
+
+    EVENT_STATUS_CHOICES = (
+        ('OP', 'Open for Registration'),
+        ('ED', 'Activity Ended'),
+    )
+    status = models.CharField(
+        max_length=2,
+        choices=EVENT_STATUS_CHOICES,
+        default='OP',
+    )
 
     def __str__(self):
         return self.title
+
+class Keyword(models.Model):
+    keyword = models.CharField(max_length=64, unique=True)
+    description = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.keyword
+
+class Event_Keyword(models.Model):
+    event = models.ForeignKey("Event", on_delete=Event)
+    keyword = models.ForeignKey("Keyword", on_delete=Keyword)
+
+    def __str__(self):
+        return "Event: {} Keyword: {}".format(event, keyword)
+
+class User_Keyword(models.Model):
+    user = models.ForeignKey("User", on_delete=User)
+    keyword = models.ForeignKey("Keyword", on_delete=Keyword)
+
+    def __str__(self):
+        return "User: {} Keyword: {}".format(user, keyword)
 
 class User_Event(models.Model):
     user = models.ForeignKey("User", on_delete=User)
